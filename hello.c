2901356c515cdeacef645bcad7b2299c3a2e215f
Luis Trejo <lbanuelostrejo2@huskers.unl.edu>
2021-08-26 23:58:02 +0000

    initial comit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Luis Trejo
+ * Date: 2021-08-26
+ * 
+ * A simple hello world program
+ */
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Luis Banuelos Trejo\n")
+    
+    return 0;
+}
