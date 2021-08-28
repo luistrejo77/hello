b3b3d28f8b540ed1a6f643c817057bd4cf1d97e5
Luis Trejo <lbanuelostrejo2@huskers.unl.edu>
2021-08-28 19:35:53 +0000

    adedd my major to the output

--- hello.c
+++ hello.c
@@ -2,14 +2,15 @@
  * Author: Luis Trejo
  * Date: 2021-08-26
  * 
- * A simple hello world program
+ * A simple hello world program in C 
  */
 
 #include<stdlib.h>
 #include<stdio.h>
 
 int main(int argc, char **argv) {
-    printf("Luis Banuelos Trejo\n")
+    printf("Hello world, I'm Luis Trejo!\n");
+    printf("My major is Computer Science");cel
     
     return 0;
 }
