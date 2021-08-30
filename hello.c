2008a7ba5ec6c4e96f75409f3c00acf82d882042
Luis Trejo <lbanuelostrejo2@huskers.unl.edu>
2021-08-30 13:34:16 +0000

    fixed a typo

--- hello.c
+++ hello.c
@@ -1,16 +1,16 @@
 /**
- * Author: Luis Trejo
- * Date: 2021-08-26
- * 
- * A simple hello world program in C 
- */
+  * Author: Luis Trejo
+  * Date: 2021-08-26
+  * lbanuelostrejo2@huskers.unl.edu
+  * 
+  */
 
 #include<stdlib.h>
 #include<stdio.h>
 
 int main(int argc, char **argv) {
     printf("Hello world, I'm Luis Trejo!\n");
-    printf("My major is Computer Science");cel
+    printf("My major is Computer Science");
     
     return 0;
 }
